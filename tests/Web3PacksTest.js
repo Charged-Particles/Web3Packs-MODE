dc62c5d0c5e94426883bea239478e29ff83c5cf7
scammi <scammi@gmail.com>
2023-01-06 11:03:22 -0300

    swap multiple assets

--- tests/Web3PacksTest.js
+++ tests/Web3PacksTest.js
@@ -13,6 +13,7 @@ describe('Web3Packs', function() {
 
   const USDcContractAddress = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174';
   const USDtContractAddress = '0xc2132D05D31c914a87C6611C10748AEb04B58e8F';
+  const UniContractAddress = '0xb33EaAd8d922B1083446DC23f610c2567fB5180f';
   const USDcWhale = '0xfa0b641678f5115ad8a8de5752016bd1359681b9';
    
   beforeEach(async () => {
@@ -46,7 +47,7 @@ describe('Web3Packs', function() {
       const ERC20SwapOrder = [{
         inputTokenAddress: USDcContractAddress,
         outputTokenAddress: USDtContractAddress,
-        inputTokenAmount: 90
+        inputTokenAmount: 10 
       }];
 
       const swapTransaction = await web3packs.swap(ERC20SwapOrder);
@@ -55,10 +56,48 @@ describe('Web3Packs', function() {
       const USDt = new ethers.Contract(USDtContractAddress, erc20Abi, whaleSigner);
       const USDtBalanceAfterSwap = await USDt.balanceOf(web3packs.address);
 
-      expect(USDtBalanceAfterSwap).to.equal(89);
+      expect(USDtBalanceAfterSwap).to.equal(9);
 
       const balanceBeforeSwap1 = await USDc.balanceOf(web3packs.address);
       console.log(balanceBeforeSwap1.toString());
+    });
+
+    it ('Swaps multiple assets', async() => {      // grant maUSD to the Web3Packs contract.
+      await network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: [ USDcWhale ],
+      });
+
+      // Deposit usdc into web3pack contract
+      const USDcWhaleSigner = await ethers.getSigner(USDcWhale);
+      const USDc = new ethers.Contract(USDcContractAddress, erc20Abi, USDcWhaleSigner);
+      const foundUSDcWeb3PacksTransaction = await USDc.transfer(web3packs.address, 100);
+      await foundUSDcWeb3PacksTransaction.wait();
+
+      const ERC20SwapOrder = [
+        {
+          inputTokenAddress: USDcContractAddress,
+          outputTokenAddress: USDtContractAddress,
+          inputTokenAmount: 10 
+        },
+        {
+          inputTokenAddress: USDcContractAddress,
+          outputTokenAddress: UniContractAddress,
+          inputTokenAmount: 10 
+        }
+      ];
+
+      const swapTransaction = await web3packs.swap(ERC20SwapOrder);
+      await swapTransaction.wait();
+
+      const USDt = new ethers.Contract(USDtContractAddress, erc20Abi, USDcWhaleSigner);
+      const UNI = new ethers.Contract(UniContractAddress, erc20Abi, USDcWhaleSigner);
+
+      const USDtBalanceAfterSwap = await USDt.balanceOf(web3packs.address);
+      const UNIBalanceAfterSwap = await UNI.balanceOf(web3packs.address);
+
+      expect(USDtBalanceAfterSwap).to.equal(18);
+      expect(UNIBalanceAfterSwap).to.equal(1659805226163);
     });
   });
 })
