62ee50d3c00f6cfbd3c02ae4a8abd8cfde74c348
scammi <scammi@gmail.com>
2023-01-19 09:52:36 -0300

    Update test for single quote

--- tests/UniSwap.ts
+++ tests/UniSwap.ts
@@ -1,9 +1,25 @@
 import { expect } from "chai";
+import { Swap } from "../uniswap/libs/types";
 import { quote } from "../uniswap/quote";
+import { WMATIC_TOKEN, USDC_TOKEN } from "../uniswap/libs/constants";
 
 describe('UniSwap', async () => {
   it ('Gets fetches single quote', async () => {
-    const quoteResult = await quote();
-    expect(quoteResult).to.be.eq('93');
+    const swap: Swap = {
+      tokens: {
+        in: WMATIC_TOKEN,
+        amountIn: 100,
+        out: USDC_TOKEN,
+        poolFee: 500,
+      },
+    };
+    
+    const quoteResult = await quote(swap);
+    expect(Number(quoteResult)).to.be.within(90,100);
+  });
+
+  it.skip ('Fetches multiple quotes using multicall', async () => {
+    // const swaps: [Swap] =  []
+    // const quotesResult = await multiQuotes(swaps);
   });
 });
--- uniswap/quote.ts
+++ uniswap/quote.ts
@@ -7,49 +7,39 @@ import { QUOTER_CONTRACT_ADDRESS, POOL_FACTORY_CONTRACT_ADDRESS } from './libs/c
 import IUniswapV3PoolABI from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json'
 import Quoter from '@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json'
 
-import { WMATIC_TOKEN, USDC_TOKEN } from './libs/constants';
 
-const CurrentConfig: Swap = {
-  tokens: {
-    in: WMATIC_TOKEN,
-    amountIn: 100,
-    out: USDC_TOKEN,
-    poolFee: 500,
-  },
-};
-
-export async function quote(): Promise<string> {
+export async function quote(swap: Swap): Promise<string> {
   const quoterContract = new ethers.Contract(
     QUOTER_CONTRACT_ADDRESS,
     Quoter.abi,
     getProvider()
   )
-  const poolConstants = await getPoolConstants()
+  const poolConstants = await getPoolConstants(swap)
 
   const quotedAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
     poolConstants.token0,
     poolConstants.token1,
     poolConstants.fee,
     fromReadableAmount(
-      CurrentConfig.tokens.amountIn,
-      CurrentConfig.tokens.in.decimals
+      swap.tokens.amountIn,
+      swap.tokens.in.decimals
     ).toString(),
     0
   )
 
-  return toReadableAmount(quotedAmountOut, CurrentConfig.tokens.out.decimals)
+  return toReadableAmount(quotedAmountOut, swap.tokens.out.decimals)
 }
 
-async function getPoolConstants(): Promise<{
+async function getPoolConstants(swap: Swap): Promise<{
   token0: string
   token1: string
   fee: number
 }> {
   const currentPoolAddress = computePoolAddress({
     factoryAddress: POOL_FACTORY_CONTRACT_ADDRESS,
-    tokenA: CurrentConfig.tokens.in,
-    tokenB: CurrentConfig.tokens.out,
-    fee: CurrentConfig.tokens.poolFee,
+    tokenA: swap.tokens.in,
+    tokenB: swap.tokens.out,
+    fee: swap.tokens.poolFee,
   })
 
   const poolContract = new ethers.Contract(
