b3488a9a91219ce3ff231d43ac3af1c83e512f35
scammi <scammi@gmail.com>
2023-03-16 15:50:12 -0300

    Indent erc721 mintable

--- contracts/lib/ERC721Mintable.sol
+++ contracts/lib/ERC721Mintable.sol
@@ -6,19 +6,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract MyToken is ERC721, Ownable {
-    using Counters for Counters.Counter;
+  using Counters for Counters.Counter;
 
-    Counters.Counter private _tokenIdCounter;
+  Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("MyTestToken", "MTK") {}
+  constructor() ERC721("MyTestToken", "MTK") {}
 
-    function safeMint(address to)
-        public
-        onlyOwner
-        returns (uint256 tokenId)
-    {
-        tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-    }
+  function safeMint(address to)
+    public
+    onlyOwner
+    returns (uint256 tokenId)
+  {
+    tokenId = _tokenIdCounter.current();
+    _tokenIdCounter.increment();
+    _safeMint(to, tokenId);
+  }
 }
