2bb17159668310674bbfe8309c3f15d254f9f44d
scammi <scammi@gmail.com>
2024-06-25 17:37:26 -0300

    Minting scripts

--- typechain-types/@uniswap/v3-core/contracts/interfaces/index.ts
+++ typechain-types/@uniswap/v3-core/contracts/interfaces/index.ts
@@ -3,6 +3,3 @@
 /* eslint-disable */
 import type * as callback from "./callback";
 export type { callback };
-import type * as pool from "./pool";
-export type { pool };
-export type { IUniswapV3Pool } from "./IUniswapV3Pool";
--- typechain-types/factories/@uniswap/v3-core/contracts/interfaces/index.ts
+++ typechain-types/factories/@uniswap/v3-core/contracts/interfaces/index.ts
@@ -2,5 +2,3 @@
 /* tslint:disable */
 /* eslint-disable */
 export * as callback from "./callback";
-export * as pool from "./pool";
-export { IUniswapV3Pool__factory } from "./IUniswapV3Pool__factory";
