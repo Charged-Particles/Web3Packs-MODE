8cdcd9f806983adbec0164ac981777ddb65542f1
scammi <scammi@gmail.com>
2024-07-08 18:16:50 -0300

    Remove log from contract

--- deploy/001_deploy_core.js
+++ deploy/001_deploy_core.js
@@ -17,31 +17,6 @@ const {
 
 const _ = require('lodash');
 
-const _ADDRESS = {
-  // Ethereum
-  1: {
-    ChargedParticles: '',
-    ChargedState: '',
-    UniswapRouter: '',
-  },
-  5: {
-    ChargedParticles: '',
-    ChargedState: '',
-    UniswapRouter: '',
-  },
-  // Polygon
-  137: {
-    ChargedParticles: '0x0288280Df6221E7e9f23c1BB398c820ae0Aa6c10',
-    ChargedState: '0x9c00b8CF03f58c0420CDb6DE72E27Bf11964025b',
-    UniswapRouter: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
-  },
-  80001: {
-    ChargedParticles: '0x51f845af34c60499a1056FCDf47BcBC681A0fA39',
-    ChargedState: '0x581c57b86fC8c2D639f88276478324cE1380979D',
-    UniswapRouter: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
-  },
-};
-
 module.exports = async (hre) => {
     const { ethers, getNamedAccounts, deployments } = hre;
     const { deploy } = deployments;
