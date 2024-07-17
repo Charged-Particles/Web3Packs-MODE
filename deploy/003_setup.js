219be4177c601581ef772629fafad28eaf27ad97
scammi <scammi@gmail.com>
2024-07-17 11:35:30 -0300

    set state charged from setup

--- deploy/003_setup.js
+++ deploy/003_setup.js
@@ -13,33 +13,27 @@ const _ADDRESS = {
   // Ethereum
   1: {
     ChargedParticles: '0xaB1a1410EA40930755C1330Cc0fB3367897C8c41',
+    ChargedState: '0x48974C6ae5A0A25565b0096cE3c81395f604140f',
     UniswapRouter: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
     Proton: '0x04d572734006788B646ce35b133Bdf7160f79995',
-  },
-  5: {
-    ChargedParticles: '0x3A9891279481bB968a8d1300C40d9279111f1CDA',
-    UniswapRouter: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
-    Proton: '0x894fe586f4BE12cDB5d107323a2f5182161C3515',
   },
   // Polygon
   137: {
     ChargedParticles: '0x0288280Df6221E7e9f23c1BB398c820ae0Aa6c10',
+    ChargedState: '0x9c00b8CF03f58c0420CDb6DE72E27Bf11964025b',
     UniswapRouter: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
     Proton: '0x1CeFb0E1EC36c7971bed1D64291fc16a145F35DC',
-  },
-  80001: {
-    ChargedParticles: '0x51f845af34c60499a1056FCDf47BcBC681A0fA39',
-    UniswapRouter: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
-    Proton: '0xC5b2d04669b6B701195F90c15C560edaa3509C92',
   },
   // Hardhat
   31337: {
     ChargedParticles: '0x0288280Df6221E7e9f23c1BB398c820ae0Aa6c10',
+    ChargedState: '0xaB1a1410EA40930755C1330Cc0fB3367897C8c41',
     UniswapRouter: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
     Proton: '0x1CeFb0E1EC36c7971bed1D64291fc16a145F35DC',
   },
   34443: {
     ChargedParticles: '0x0288280Df6221E7e9f23c1BB398c820ae0Aa6c10',
+    ChargedState: '0x2691B4f4251408bA4b8bf9530B6961b9D0C1231F',
     UniswapRouter: '0xAc48FcF1049668B285f3dC72483DF5Ae2162f7e8',
     Proton: '0x76a5df1c6F53A4B80c8c8177edf52FBbC368E825',
   },
@@ -81,6 +75,10 @@ module.exports = async (hre) => {
 
     await executeTx('1-e', 'Web3Packs: Set proton address ', async () =>
       await web3Packs.setProton(_ADDRESS[chainId].Proton)
+    );
+
+    await executeTx('1-e', 'Web3Packs: Set proton address ', async () =>
+      await web3Packs.setChargedState(_ADDRESS[chainId].chargedState)
     );
 
     // await executeTx('1-d', 'Web3Packs: Transfer Contract Ownership', async () =>
