00ad6274bb36c950e43ca6295537b58c27f84f93
scammi <scammi@gmail.com>
2024-07-25 18:58:57 -0300

    unbundle

--- scripts/unbundle.ts
+++ scripts/unbundle.ts
@@ -7,30 +7,30 @@ const hre = require("hardhat");
 // npx hardhat run scripts/test.ts --network polygon
 async function main() {
   const accounts = await hre.ethers.getSigners();
-  const web3Packs: Web3Packs = await hre.ethers.getContract('Web3Packs');
+  const web3Packs: Web3Packs = await hre.ethers.getContract('Web3PacksMode');
 
-  const protonTokenToUnbundle = 638;
-  const uniV3NFT = '0xC36442b4a4522E871399CD717aBDD847Ab11FE88';
+  const protonTokenToUnbundle = 5;
+  const LP_NFT = '0x2e8614625226D26180aDf6530C3b1677d3D7cf10';
 
-  const chargedStateContractAddress = '0x9c00b8CF03f58c0420CDb6DE72E27Bf11964025b';
+  const chargedStateContractAddress = globals.chargedStateContractAddress;
   const chargedState = new Contract(chargedStateContractAddress, chargedStateAbi, accounts[0]);
 
   // setBreakBondApproval
   await chargedState.setApprovalForAll(
-    globals.protonPolygon,
+    globals.protonMode,
     protonTokenToUnbundle,
     web3Packs.address
   ).then((tx) => tx.wait());
 
   await web3Packs.unbundle(
     await accounts[0].getAddress(),
-    globals.protonPolygon,
+    globals.protonMode,
     protonTokenToUnbundle,
     {
       erc20TokenAddresses: [],
       nfts: [{
-        tokenAddress: uniV3NFT, // nested in the web3 pack token
-        id: 1893906
+        tokenAddress: LP_NFT, // nested in the web3 pack token
+        id: 50535
       }],
     }
   );
